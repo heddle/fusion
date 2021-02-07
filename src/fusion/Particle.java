6fc57df1dd5983c66c76793372274b768a8eed78
heddle <david.heddle@cnu.edu>
2021-02-07 08:24:07 -0500

    added Model class

--- src/fusion/Particle.java
+++ src/fusion/Particle.java
@@ -2,8 +2,18 @@ package fusion;
 
 public class Particle {
 	
+	/** code for Hydrogen (proton) */
+	public static final byte H_1 = 0;
+	
+	/** code for dueterium */
+	public static final byte H_2 = 1;
+	
+	
 	public static final Particle H1 = new Particle(1, 1, Constants.Mp, "H<SUB>1</SUB>");
 	public static final Particle H2 = new Particle(1, 2, Constants.Md, "H<SUB>2</SUB>");
+	
+	/** pre built constant particles */
+	public static final Particle particles[] = {H1, H2};
 	
 	/** the Z symbol, e.g. "H" for Z = 1, "He for Z = 2, etc" */ 
 	public String symbol;
@@ -11,7 +21,6 @@ public class Particle {
 	/** the charge, e.g. 1 for Hydrogen */
 	public int Z;
 	
-
 	/** the atomic number, e.g. 1 for proton, 2 for dueterium */
 	public int A; 
 	
@@ -19,7 +28,14 @@ public class Particle {
 	public double m;
 	
 
-	public Particle(int Z, int A, double m, String symbol) {
+	/**
+	 * Create a particle
+	 * @param Z
+	 * @param A
+	 * @param m
+	 * @param symbol
+	 */
+	private Particle(int Z, int A, double m, String symbol) {
 		this.Z = Z;
 		this.A = A;
 		this.m = m;
